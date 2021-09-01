d377176d63587f08a11948278b26451675468462
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-09-02 00:01:28 +0530

    revamped project

--- app.js
+++ app.js
@@ -1,73 +1,53 @@
-let translate_type=document.querySelector("#translate_type")
-const select=document.querySelector("#select")
-let chosen_type=document.querySelector("#chosen_type")
-
-let input_text=document.querySelector("#textarea");
-const translator=document.querySelector("#translator");
+const convertThis=document.querySelector("#convertThis");
+let userInput=document.querySelector("#userInput");
 let output=document.querySelector("#output");
-const box2=document.querySelector("#box2")
 
-// ===========================storage========================
-let storageURL=[
-{k:"yoda",u:"https://api.funtranslations.com/translate/yoda.json"},
-{k:"morse",u:"https://api.funtranslations.com/translate/morse.json"},
-{k:"mando'a",u:"https://api.funtranslations.com/translate/mandalorian.json"},
-{k:"avatar",u:"https://api.funtranslations.com/translate/navi.json"}
-]
-
+let contentBox=document.querySelector(".container")
+contentBox.style.display="none"
 let serverURL;
-// =========================translator chooser=================
-select.addEventListener("click", function () {
+
+
+let storageURL={
+    yoda:"https://api.funtranslations.com/translate/yoda.json",
+    morse:"https://api.funtranslations.com/translate/morse.json",
+    "MANDO'A":"https://api.funtranslations.com/translate/mandalorian.json",
+    avatar:"https://api.funtranslations.com/translate/navi.json"
+}
+
+let translators=document.querySelector("#translators")
+translators.addEventListener("change",(e)=>{
+    let chosenTranslator=e.target.value;
     output.innerHTML=''
-    let keys=translate_type.value;
-    box2.style.display="none";
-    if(keys!=''){
-        for(let i=0;i<storageURL.length;i++){
-            console.log(storageURL[i])
-            let holder=storageURL[i].k;
-            if(keys.toUpperCase()===holder.toUpperCase()){
-                chosen_type.innerHTML=`${keys} translator enabled.`
-                output.innerHTML=''
-                serverURL=storageURL[i].u
-                box2.style.display="grid";
-                return serverURL
-            }
-            chosen_type.innerHTML="We do not have this translator yet.";
-        }   
-    }else{
-        return chosen_type.innerHTML="Please select a translator from below.";
+    for(let eachKey in storageURL){
+        if(chosenTranslator.toUpperCase()===eachKey.toUpperCase()){
+            serverURL=storageURL[eachKey];
+            contentBox.style.display="flex"
+        }
     }
-});
-// ======================fina url creator==========================
+})
+
+
 function createURL(input){
    return serverURL +"?"+"text=" +input;
 }
 
 function errorHandler(error) {
-    input_text.value=''
     console.log("error occured", error);
     output.innerHTML=`Something is wrong with the server ! Please try again later.`
 }
-translator.addEventListener("click", ()=>{
-    if(input_text.value.length<1){
+
+
+convertThis.addEventListener("click", ()=>{
+    if(userInput.value.length<1){
         output.innerHTML = "Please enter some data to translate it.";
         setTimeout(()=>{
             output.innerHTML = '';
         },2000)
     }
     else{
-        translator.value="Translating..."
-        setTimeout(()=>{
-            translator.value = "Translate";
-        },1500)
-        fetch(createURL(input_text.value))
+        fetch(createURL(userInput.value))
         .then(response =>response.json())
-        .then(json=>{
-        output.innerText = json.contents.translated;
-        })
+        .then(json=>output.innerText = json.contents.translated)
         .catch(errorHandler)
     }
 })
-
-
-
